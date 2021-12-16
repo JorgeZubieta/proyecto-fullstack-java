ba07786084dc8f093da98deb3e1c68b9989e6199
jorge zubieta <zubietajor@gmail.com>
2021-12-16 18:38:05 -0300

    inicializamos mi proyecto

---
+++ src/main/java/modelo/Usuarios.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author jorgezubieta
+ */
+public class Usuarios {
+    private int id;
+    private String nombre;
+    private String apellidp;
+    private String mail;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidp() {
+        return apellidp;
+    }
+
+    public void setApellidp(String apellidp) {
+        this.apellidp = apellidp;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+    
+    
+}
---
+++ src/main/webapp/Vistas/usurarios.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : usurarios
+    Created on : 16 dic. 2021, 16:20:17
+    Author     : jorgezubieta
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
