9792e767a3368534ad4309a1ddf72cb7e536a814
jorge zubieta <zubietajor@gmail.com>
2021-12-27 10:40:37 -0300

    correccion get apellido en archovos

--- src/main/java/modelo/Usuarios.java
+++ src/main/java/modelo/Usuarios.java
@@ -21,38 +21,34 @@ public class Usuarios {
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
 
+    
     public String getNombre() {
         return nombre;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    
     public String getApellido() {
         return apellido;
     }
-
-    public void setApellido(String apellidp) {
-        this.apellido = apellidp;
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
     }
 
+    
     public String getMail() {
         return mail;
     }
-
     public void setMail(String mail) {
         this.mail = mail;
     }
-
 //    String getApellido() {
 //        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-    
-    
+//    }   
 }
